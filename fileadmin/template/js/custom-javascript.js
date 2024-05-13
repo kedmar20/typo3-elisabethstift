1df3ad5596ec9a37c88121c868cbb71d7c1c9bef
Webserver User <764811@sh22908.ispgateway.de>
2024-05-13 08:52:37 +0200

    add font-class to footer-menu trough javascript

---
+++ fileadmin/template/js/custom-javascript.js
@@ -0,0 +1,5 @@
+console.log("hello JavaScript");
+/*Footer-Menu*/
+const fotterMenuUl = document.querySelector("#c56 > div > div > div > div > ul");
+console.log(fotterMenuUl);
+fotterMenuUl.classList.add("catamaran-22-medium");
