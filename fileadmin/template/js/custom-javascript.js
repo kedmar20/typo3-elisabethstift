b0b9124df40250b552ba3fdab742f6490040bb33
Webserver User <764811@sh22908.ispgateway.de>
2024-05-24 07:12:01 +0200

    change 3-Columns-Section and start setup News-List at HomePage

--- fileadmin/template/js/custom-javascript.js
+++ fileadmin/template/js/custom-javascript.js
@@ -1,79 +1,79 @@
-if (screen.width > 1150){ 
-console.log("hello JavaScript");
-/*Footer-Menu*/
-const fotterMenuUl = document.querySelector("#c56 > div > div > div > div > ul");
-fotterMenuUl.classList.add("catamaran-22-medium");
+if (screen.width > 1150) {
+   console.log("hello JavaScript");
+   /*Footer-Menu*/
+   const fotterMenuUl = document.querySelector("#c56 > div > div > div > div > ul");
+   fotterMenuUl.classList.add("catamaran-22-medium");
 
-/*Dropdownmenu on hover*/
-const hauptmenuItemAll = [...document.querySelectorAll("#mainnavigation > ul > li")];
-const header1 = document.querySelector("header");
-const dropdownContainer = document.querySelectorAll(".dropdown-menu");
-console.log(dropdownContainer);
+   /*Dropdownmenu on hover*/
+   const hauptmenuItemAll = [...document.querySelectorAll("#mainnavigation > ul > li")];
+   const header1 = document.querySelector("header");
+   const dropdownContainer = document.querySelectorAll(".dropdown-menu");
+   console.log(dropdownContainer);
 
-hauptmenuItemAll.forEach((elemHauptMenuItem) => {
-   const megaMenuContainer = elemHauptMenuItem.querySelector(".dropdown-menu.dropdown-menu-simple");
+   hauptmenuItemAll.forEach((elemHauptMenuItem) => {
+      const megaMenuContainer = elemHauptMenuItem.querySelector(".dropdown-menu.dropdown-menu-simple");
 
-   elemHauptMenuItem.addEventListener("mouseover", (event) => {
-      /*setTimeout(function() {*/
-      if (!!megaMenuContainer) {
-         megaMenuContainer.classList.add("show");
-         megaMenuContainer.setAttribute("data-bs-popper", "static");
+      elemHauptMenuItem.addEventListener("mouseover", (event) => {
+         /*setTimeout(function() {*/
+         if (!!megaMenuContainer) {
+            megaMenuContainer.classList.add("show");
+            megaMenuContainer.setAttribute("data-bs-popper", "static");
 
-         const arrayWithoutElemhauptmenuitem = hauptmenuItemAll.filter((item) => {
-            return item !== elemHauptMenuItem;
-         });
-         arrayWithoutElemhauptmenuitem.forEach((e) => {
-            const megaMenuContainerNotActiv = e.querySelector(".dropdown-menu.dropdown-menu-simple");
-            if (!!megaMenuContainerNotActiv) {
-               megaMenuContainerNotActiv.classList.remove("show");
-               megaMenuContainerNotActiv.removeAttribute("data-bs-popper", "static");
-            }
-         });
-      }
-      /*}, 200);                       */
+            const arrayWithoutElemhauptmenuitem = hauptmenuItemAll.filter((item) => {
+               return item !== elemHauptMenuItem;
+            });
+            arrayWithoutElemhauptmenuitem.forEach((e) => {
+               const megaMenuContainerNotActiv = e.querySelector(".dropdown-menu.dropdown-menu-simple");
+               if (!!megaMenuContainerNotActiv) {
+                  megaMenuContainerNotActiv.classList.remove("show");
+                  megaMenuContainerNotActiv.removeAttribute("data-bs-popper", "static");
+               }
+            });
+         }
+         /*}, 200);                       */
+      });
+
+      header1.addEventListener("mouseleave", (e) => {
+         /*setTimeout(function() {*/
+         if (!!megaMenuContainer && !elemHauptMenuItem.classList.contains("active")) {
+            megaMenuContainer.classList.remove("show");
+            megaMenuContainer.removeAttribute("data-bs-popper", "static");
+         }
+         /*}, 200);*/
+      });
    });
 
-   header1.addEventListener("mouseleave", (e) => {
-      /*setTimeout(function() {*/
-      if (!!megaMenuContainer && !elemHauptMenuItem.classList.contains("active")) {
-         megaMenuContainer.classList.remove("show");
-         megaMenuContainer.removeAttribute("data-bs-popper", "static");
-      }
-      /*}, 200);*/
-   });
-});
-
-/*scroll event + smaller Header*/
-window.addEventListener("scroll", (e) => {
-   if (!!(window.scrollY > 5)) {
-      header1.classList.remove("header-scroll-null");
-      header1.classList.add("header-scroll");
-      /*header1.style.paddingTop = "10px";
+   /*scroll event + smaller Header*/
+   window.addEventListener("scroll", (e) => {
+      if (!!(window.scrollY > 5)) {
+         header1.classList.remove("header-scroll-null");
+         header1.classList.add("header-scroll");
+         /*header1.style.paddingTop = "10px";
       header1.style.paddingBottom = "10px";
       header1.style.height = "8vw";
       header1.style.maxHeight = "8vw";*/
-      !!dropdownContainer
-         ? dropdownContainer.forEach((item) => {
-              /* item.style.top = "8vw";*/
-              item.classList.add("dropdown-scroll");
-           })
-         : "";
-   } else {
-      header1.classList.remove("header-scroll");
-      header1.classList.add("header-scroll-null");
-      !!dropdownContainer
-         ? dropdownContainer.forEach((item) => {
-              item.classList.remove("dropdown-scroll");
-           })
-         : "";
-   }
-});
+         !!dropdownContainer
+            ? dropdownContainer.forEach((item) => {
+                 /* item.style.top = "8vw";*/
+                 item.classList.add("dropdown-scroll");
+              })
+            : "";
+      } else {
+         header1.classList.remove("header-scroll");
+         header1.classList.add("header-scroll-null");
+         !!dropdownContainer
+            ? dropdownContainer.forEach((item) => {
+                 item.classList.remove("dropdown-scroll");
+              })
+            : "";
+      }
+   });
 
-/*homepage>section-3columns, clip-Path border*/
-const parentOf3columnsWrapper = document.querySelector("#page-content > div:nth-child(3) > div");
-//const newSvgElement = document.createElement("svg");
-//newSvgElement.innerHTML = `
-parentOf3columnsWrapper.innerHTML += `
+   /*homepage>section-3columns, clip-Path border*/
+   const parentOf3columnsWrapper = document.querySelector("#page-content > div:nth-child(3) > div");
+   //const newSvgElement = document.createElement("svg");
+   //newSvgElement.innerHTML = `
+   parentOf3columnsWrapper.innerHTML += `
 <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" viewBox="0 0 1920 361.156">
 <defs>
 <clipPath id="wave">
@@ -82,9 +82,9 @@ parentOf3columnsWrapper.innerHTML += `
   </defs>
 </svg>
 `;
-//parentOf3columnsWrapper.appendChild(newSvgElement);
-setTimeout(() => {
+   //parentOf3columnsWrapper.appendChild(newSvgElement);
+   /*setTimeout(() => {
    const sectionRow = parentOf3columnsWrapper.querySelector(".section-row");
    sectionRow.style.clipPath = "url(#wave)";   
-}, 100);
+}, 100);*/
 }
